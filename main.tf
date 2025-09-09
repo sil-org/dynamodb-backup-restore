edc4b91650cbcfc1ceaeca69a9791f9ae10b7777
Praveen <praveen_koilraj@sil.org>
2025-09-09 19:23:32 +0530

    Update main.tf
    Co-authored-by: forevermatt <forevermatt@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -46,7 +46,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "mfa_backups" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
-    bucket_key_enabled = true
   }
 }
 
